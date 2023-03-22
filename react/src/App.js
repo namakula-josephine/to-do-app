ec68f52e2f613526d651e511f9e829475cc7715d
wakabibrian <wakabibrian95@gmail.com>
2023-03-23 00:00:56 +0300

    added material-ui icons

--- react/src/App.js
+++ react/src/App.js
@@ -1,8 +1,9 @@
-import Tasks from "./pages/tasks/Tasks";
+// import Tasks from "./pages/tasks/Tasks";
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
 
 function App() {
   return (
-    <Tasks />
+    <AccessibilityNewIcon />
   );
 }
 
