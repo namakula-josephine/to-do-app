9e24b8fbfaa965911c9e40d768b604d533085a52
wakabibrian <wakabibrian95@gmail.com>
2023-03-23 00:47:13 +0300

    added settings and new tasks icon

--- react/src/components/topbar/Topbar.js
+++ react/src/components/topbar/Topbar.js
@@ -1,7 +1,19 @@
+import "./topbar.css"
+import AddIcon from '@mui/icons-material/Add';
+import SettingsIcon from '@mui/icons-material/Settings';
+
 export default function Topbar(){
     return(
-        <div>
-            Topbar
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">Your To Do</span>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarIcons">
+                    <AddIcon />
+                    <SettingsIcon />
+                </div>
+            </div>
         </div>
     )
 }
