70d37034e0f13a6e752c5a5ed5095681ef2a5e4c
wakabibrian <wakabibrian95@gmail.com>
2023-03-26 01:12:13 +0300

    Updated menu style

--- src/components/menubar/Menubar.js
+++ src/components/menubar/Menubar.js
@@ -1,48 +1,46 @@
 import "./Menubar.css";
-import HomeIcon from '@mui/icons-material/Home';
-import StarOutlineIcon from '@mui/icons-material/StarOutline';
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CancelIcon from '@mui/icons-material/Cancel';
+import HomeIcon from "@mui/icons-material/Home";
+import GradeIcon from '@mui/icons-material/Grade';
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 function Menubar() {
   return (
     <div className="menuBar">
-      <div className="menuBarWrapper">
-        <ul className="menuBarList">
-          <li className="menuBarListItem menuBarActive">
-            <HomeIcon className="menuBarIcon"/>
-            <span className="menuBarListItemText">All Tasks</span>
-            <span className="menuBarListItemCounter">12</span>
-          </li>
-          <li className="menuBarListItem">
-            <StarOutlineIcon className="menuBarIcon"/>
-            <span className="menuBarListItemText">Important</span>
-            <span className="menuBarListItemCounter">2</span>
-          </li>
-          <li className="menuBarListItem">
-            <CalendarTodayIcon className="menuBarIcon"/>
-            <span className="menuBarListItemText">Today</span>
-            <span className="menuBarListItemCounter">4</span>
-          </li>
-          <li className="menuBarListItem">
-            <CalendarMonthIcon className="menuBarIcon"/>
-            <span className="menuBarListItemText">This Week</span>
-            <span className="menuBarListItemCounter">5</span>
-          </li>
-          <li className="menuBarListItem">
-            <CheckCircleIcon className="menuBarIcon"/>
-            <span className="menuBarListItemText">Completed</span>
-            <span className="menuBarListItemCounter">3</span>
-          </li>
-          <li className="menuBarListItem">
-            <CancelIcon className="menuBarIcon"/>
-            <span className="menuBarListItemText">Missed</span>
-            <span className="menuBarListItemCounter">2</span>
-          </li>
-        </ul>
-      </div>
+      <ul className="menuBarList">
+        <li className="menuBarListItem menuBarListActive">
+          <HomeIcon className="menuBarIcon" />
+          <span className="menuBarListItemText">All Tasks</span>
+          <span className="menuBarListItemCounter">12</span>
+        </li>
+        <li className="menuBarListItem">
+          <GradeIcon className="menuBarIcon" />
+          <span className="menuBarListItemText">Important</span>
+          <span className="menuBarListItemCounter">2</span>
+        </li>
+        <li className="menuBarListItem">
+          <CalendarTodayIcon className="menuBarIcon" />
+          <span className="menuBarListItemText">Today</span>
+          <span className="menuBarListItemCounter">4</span>
+        </li>
+        <li className="menuBarListItem">
+          <CalendarMonthIcon className="menuBarIcon" />
+          <span className="menuBarListItemText">This Week</span>
+          <span className="menuBarListItemCounter">5</span>
+        </li>
+        <li className="menuBarListItem">
+          <CheckCircleIcon className="menuBarIcon" />
+          <span className="menuBarListItemText">Completed</span>
+          <span className="menuBarListItemCounter">3</span>
+        </li>
+        <li className="menuBarListItem">
+          <CancelIcon className="menuBarIcon" />
+          <span className="menuBarListItemText">Missed</span>
+          <span className="menuBarListItemCounter">2</span>
+        </li>
+      </ul>
     </div>
   );
 }
