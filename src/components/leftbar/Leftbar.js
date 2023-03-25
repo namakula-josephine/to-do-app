95f3c048cec342def51edea28d82e1f8356d6393
wakabibrian <wakabibrian95@gmail.com>
2023-03-25 19:58:12 +0300

    Added list item on left bar

--- src/components/leftbar/Leftbar.js
+++ src/components/leftbar/Leftbar.js
@@ -1,10 +1,17 @@
 import "./Leftbar.css";
+import HomeIcon from '@mui/icons-material/Home';
 
 function Leftbar() {
   return (
     <div className="leftBar">
       <div className="leftBarWrapper">
-        
+        <ul className="leftBarList">
+          <li className="leftBarListItem">
+            <HomeIcon className=""/>
+            <span className="leftBarListItemText">All Tasks</span>
+            <span className="leftBarListItemCounter">9</span>
+          </li>
+        </ul>
       </div>
     </div>
   );
