eb75ac1fcc389f5a451897aa9d693428f7c8e8ce
Segawa_Kevin <ksegawa750@gmail.com>
2023-03-27 21:13:43 +0300

    Just edited the Right bar

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -1,7 +1,7 @@
 import "./Rightbar.css";
 
 function Rightbar() {
-  return <div className="rightBar">click on</div>;
+  return <div className="rightBar">click on app</div>;
 }
 
 export default Rightbar;
