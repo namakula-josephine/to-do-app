a0abc1d225c3ae3c5f650a892a82dde30394a9bb
Segawa_Kevin <ksegawa750@gmail.com>
2023-03-28 10:21:32 +0300

    Edited the Right Bar

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -1,7 +1,7 @@
 import "./Rightbar.css";
 
 function Rightbar() {
-  return <div className="rightBar">click the app</div>;
+  return <div className="rightBar">click the Task Title to view details</div>;
 }
 
 export default Rightbar;
