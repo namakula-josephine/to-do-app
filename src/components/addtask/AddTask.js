3b1d13c58205f9bd0d7738bd0cb4e051f13a8eef
wakabibrian <wakabibrian95@gmail.com>
2023-03-29 16:37:16 +0300

    Added AddTask Component

---
+++ src/components/addtask/AddTask.js
@@ -0,0 +1,15 @@
+import "./AddTask.css";
+
+export default function AddTask() {
+  return (
+    <div className="addTask">
+      <div className="addTaskTitle">Title</div>
+      <div className="addTaskForm">
+        <div className="addTaskFormTitle">Form Title</div>
+        <div className="addTaskFormDescription">Form Description</div>
+        <div className="addTaskFormSchedule">Form Description</div>
+      </div>
+      <button className="addTaskButton">Button</button>
+    </div>
+  );
+}
