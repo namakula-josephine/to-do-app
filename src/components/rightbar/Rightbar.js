fed6c3df627a32a0769c0025baef8bd74a25fdc2
Segawa_Kevin <ksegawa750@gmail.com>
2023-04-01 09:31:48 +0300

    edited files

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -3,7 +3,7 @@ import "./Rightbar.css";
 function Rightbar() {
   return (
   <div>
-    <img src="/assets/click_me.webp" alt="click prompt" className="clickIcon" />
+    <img src="/assets/click_me.webp" alt="Click me" className="clickIcon" />
     <div className="rightBar">click the Task Title to view details.</div>
   </div>
 
