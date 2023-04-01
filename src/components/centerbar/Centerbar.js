7a9d753a385b855e0bb5cdd550e5ecf92d2d782a
Segawa_Kevin <ksegawa750@gmail.com>
2023-04-01 11:00:40 +0300

    added text to center bar

--- src/components/centerbar/Centerbar.js
+++ src/components/centerbar/Centerbar.js
@@ -1,7 +1,16 @@
 import "./Centerbar.css";
+import HomeIcon from "@mui/icons-material/Home";
+import GradeIcon from "@mui/icons-material/Grade";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 function Centerbar() {
-  return <div className="centerBar">Centerbar</div>;
+  return <div className="centerBar">
+      <HomeIcon className="menuBarIconn"/>
+      <span className="menuBarListItemTextt">All Tasks</span>
+    </div>;
 }
 
 export default Centerbar;
